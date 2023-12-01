c8163fe4db0988337f6b32b1d5248b9e8a616845
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2023-12-01 09:18:44 +0100

    Day1

--- src/main/scala/org/lafeuille/adventofcode/y2023/d01/Day01.scala
+++ src/main/scala/org/lafeuille/adventofcode/y2023/d01/Day01.scala
@@ -12,14 +12,33 @@ object Day01 {
 
 object Part1 extends App {
 
-  def result(list: List[String]): Int = 0
+  def result(list: List[String]): Int = list
+    .map(line => line.filter(_.isDigit))
+    .map(digits => "" + digits.head + digits.last)
+    .map(digits => digits.toInt)
+    .sum
 
   println(result(Day01.myList()))
 }
 
 object Part2 extends App {
 
-  def result(list: List[String]): Int = 0
+  private def rec(string: String): String = string match {
+    case "" => ""
+    case s if s.startsWith("one") => "1" + rec(s.substring(3))
+    case s if s.startsWith("two") => "2" + rec(s.substring(3))
+    case s if s.startsWith("three") => "3" + rec(s.substring(5))
+    case s if s.startsWith("four") => "4" + rec(s.substring(4))
+    case s if s.startsWith("five") => "5" + rec(s.substring(4))
+    case s if s.startsWith("six") => "6" + rec(s.substring(3))
+    case s if s.startsWith("seven") => "7" + rec(s.substring(5))
+    case s if s.startsWith("eight") => "8" + rec(s.substring(5))
+    case s if s.startsWith("nine") => "9" + rec(s.substring(4))
+    case s => s.take(1) + rec(s.drop(1))
+  }
+
+  def result(list: List[String]): Int =
+    Part1.result(list.map(rec))
 
   println(result(Day01.myList()))
 }
--- src/test/scala/org/lafeuille/adventofcode/y2023/d01/Day01Spec.scala
+++ src/test/scala/org/lafeuille/adventofcode/y2023/d01/Day01Spec.scala
@@ -4,20 +4,52 @@ package org.lafeuille.adventofcode.y2023.d01
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.*
 
-val sample = Nil
-
 class Part1Spec extends AnyFlatSpec with should.Matchers {
 
-  val sampleResult = 0
+  val sample = List(
+    "1abc2",
+    "pqr3stu8vwx",
+    "a1b2c3d4e5f",
+    "treb7uchet"
+  )
+
+  val sampleResult = 142
 
   "Sample example" should s"have result $sampleResult" in {
     Part1.result(sample) shouldBe sampleResult
   }
 
-  val inputResult = 0
+  val inputResult = 53386
 
   "My input" should s"have result $inputResult" in {
     Part1.result(Day01.myList()) shouldBe inputResult
+  }
+
+}
+
+
+class Part2Spec extends AnyFlatSpec with should.Matchers {
+
+  val sample = List(
+    "two1nine",
+    "eightwothree",
+    "abcone2threexyz",
+    "xtwone3four",
+    "4nineeightseven2",
+    "zoneight234",
+    "7pqrstsixteen"
+  )
+
+  val sampleResult = 281
+
+  "Sample example" should s"have result $sampleResult" in {
+    Part2.result(sample) shouldBe sampleResult
+  }
+
+  val inputResult = 53293
+
+  "My input" should s"have result $inputResult" in {
+    Part2.result(Day01.myList()) shouldBe inputResult
   }
 
 }
